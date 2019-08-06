679e226635e10462a211a4b339dc5b97bdc77c4f
taastingo <44228598+taastingo@users.noreply.github.com>
2019-08-07 03:38:34 +0530

    Order System

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Order
+public class Order : MonoBehaviour
 {
     public string plantName;
     public int orderDeadline;
     public Sprite orderImage;
+    public int currentTick;
+     GameObject UIElement;
 
     public Order(string name, int deadline, Sprite image)
     {
@@ -15,5 +17,28 @@ public class Order
         orderDeadline = deadline;
         orderImage = image;
     }
+    void Start()
+    {
+        
 
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setUIElement(GameObject obj)
+    {
+        UIElement = obj;
+    }
+
+    public void orderCompleted()
+    {
+        Destroy(UIElement);
+    }
+
+    
+    //add a timer, create an event called orderNotFulFilled
+    //listen orderNotFulfilled in Boss/Plants/etc.
 }
