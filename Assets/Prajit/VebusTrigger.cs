f8fa408b4f9b42c3465bd98f9ae858b0ff63327a
prajit33 <prajit_n@nid.edu>
2019-08-08 13:23:51 +0530

    venus fly trap working
    working under a triggger box

---
+++ Assets/Prajit/VebusTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VebusTrigger : MonoBehaviour
+{
+    VenusFlyTrap _venusFlyTrap;
+    public bool playerSpotted;
+
+    public GameObject _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            playerSpotted = true;
+            _player =  collision.gameObject;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        playerSpotted = true;
+        _player = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerSpotted = false;
+            _player = null;
+        }
+    }
+
+    public GameObject returnPlayer()
+    {
+        return _player;
+    }
+
+    public bool returnPlayerSpotted()
+    {
+        return playerSpotted;
+    }
+}
