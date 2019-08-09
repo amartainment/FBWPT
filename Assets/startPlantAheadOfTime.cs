8c614b3545e745de76fff1ad2372f52a520e543a
Amar <amar.ravi@outlook.com>
2019-08-09 14:08:05 +0530

    Level2 Done

---
+++ Assets/startPlantAheadOfTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startPlantAheadOfTime : MonoBehaviour
+
+{
+    public ClimberBehavior climber;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
