e663cc24b3fd50b3e0e87d002d7c503036e1a999
Amar <amar.ravi@outlook.com>
2019-08-09 13:09:35 +0530

    Tutorial Level Complete!

--- Assets/playerManager.cs
+++ Assets/playerManager.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class playerManager : MonoBehaviour
 {
 
-    
+    public Vector3 player1Position;
+    public Vector3 player2Position;
+    public Vector3 player3Position;
+    public Vector3 player4Position;
+
     public static int[] playerNumbers = { 1, 2, 3, 4 };
     
         // Start is called before the first frame update
@@ -41,21 +45,25 @@ public class playerManager : MonoBehaviour
             case 1:
                 playerSprite.color = new Color32(255, 255, 255, 255);
                 //set player 1 to top left
-                playerSprite.gameObject.transform.position = new Vector3(-7, 4, 0);
+                // playerSprite.gameObject.transform.position = new Vector3(-7, 4, 0);
+                playerSprite.gameObject.transform.position = player1Position;
                 break;
             case 2:
                 playerSprite.color = new Color32(255, 0, 0, 255);
-                playerSprite.gameObject.transform.position = new Vector3(8, 4, 0);
+                //playerSprite.gameObject.transform.position = new Vector3(8, 4, 0);
+                playerSprite.gameObject.transform.position = player2Position;
 
                 break;
             case 3:
                 playerSprite.color = new Color32(255, 255, 0, 255);
-                playerSprite.gameObject.transform.position = new Vector3(-7, -2.5f, 0);
+               // playerSprite.gameObject.transform.position = new Vector3(-7, -2.5f, 0);
+                playerSprite.gameObject.transform.position = player3Position;
 
                 break;
             case 4:
                 playerSprite.color = new Color32(0, 255, 255, 255);
-                playerSprite.gameObject.transform.position = new Vector3(8, -2.5f, 0);
+                //playerSprite.gameObject.transform.position = new Vector3(8, -2.5f, 0);
+                playerSprite.gameObject.transform.position = player4Position;
 
                 break;
         }
